94cba71daf7a10fe522f35761d2c10b3c5052556
jonathanhoy <jonathanhoy5@gmail.com>
2018-08-08 22:37:05 -0400

    Fixed img src path

--- JonathanHoy-Project2/dev/scripts/main.js
+++ JonathanHoy-Project2/dev/scripts/main.js
@@ -35,7 +35,7 @@ $(function (){
 	let shuffledCardArray = shuffle(cardArray);
 	let shuffledCardList = '';
 	for (let i = 0; i < shuffledCardArray.length; i++) {
-		shuffledCardList += `<li class="card"><div class="card"><div class="front"></div><div class="back ${shuffledCardArray[i]}"><img src="../dev/assets/${shuffledCardArray[i]}.png" alt="A cute picture of ${shuffledCardArray[i]}."></div></div></li>`;
+		shuffledCardList += `<li class="card"><div class="card"><div class="front"></div><div class="back ${shuffledCardArray[i]}"><img src="dev/assets/${shuffledCardArray[i]}.png" alt="A cute picture of ${shuffledCardArray[i]}."></div></div></li>`;
 	};
 	$('.cards').html(shuffledCardList);
 	console.log(shuffledCardArray); // to cheat and see the cards
@@ -99,7 +99,7 @@ $(function (){
 			let shuffledCardArray = shuffle(cardArray);
 			let shuffledCardList = '';
 			for (let i = 0; i < shuffledCardArray.length; i++) {
-				shuffledCardList += `<li class="card"><div class="card"><div class="front hard"></div><div class="back ${shuffledCardArray[i]}"><img src="../dev/assets/${shuffledCardArray[i]}.png" alt="A cute picture of ${shuffledCardArray[i]}."></div></div></li>`;
+				shuffledCardList += `<li class="card"><div class="card"><div class="front hard"></div><div class="back ${shuffledCardArray[i]}"><img src="dev/assets/${shuffledCardArray[i]}.png" alt="A cute picture of ${shuffledCardArray[i]}."></div></div></li>`;
 			};
 			$('.cards').html(shuffledCardList);
 			console.log(shuffledCardArray);
